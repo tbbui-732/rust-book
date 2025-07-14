24b299a53966b5902d2707f9bee15e1e7eacbcb1
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-14 13:48:24 -0400

    continuously run a loop and give hints to the player

--- chapter_two/guessing_game/src/main.rs
+++ chapter_two/guessing_game/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let min_range: u32 = 1;
+    let max_range: u32 = 100;
+    let secret_number: u32 = rand::rng()
+                            .random_range(min_range..=max_range);
+
+    println!("guess a number between {min_range} to {max_range}");
+    println!("input: ");
+
+    loop {
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = guess.trim()
+                            .parse()
+                            .expect("please type a number!");
+
+        println!("you guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => println!("that's the right answer!"),
+        }
+    }
 }
