9780bfc76f75274060db6091b4583764ee7d52f0
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-16 15:47:25 -0400

    Function returns can give back ownership

--- chapter_four/ownership/src/main.rs
+++ chapter_four/ownership/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let s = String::from("hello");
-    take_ownership(s); // s moves to function
+    /*
+     * UNCOMMENT TO TEST
+    let s1: String = String::from("hello");
+    take_ownership(s1); // s moves to function
     println!("{s}"); // should produce an error
+    */  
+
+    let mut s2: String = String::from("goodbye");
+    s2 = take_and_give_back_ownership(s2);
+    println!("{s2}");
 }
 
 fn take_ownership(some_string: String) {
     println!("{some_string}");
 } // some_string goes out of scope and 'drop' is called
+
+fn take_and_give_back_ownership(some_string: String) -> String {
+    println!("{some_string}");
+    some_string
+}
